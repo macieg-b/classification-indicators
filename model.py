4f60ec82a15665796e691bd1ee5e78dbe81a3389
macieg-b <bartlomiejczyk.maciej@gmail.com>
2017-12-05 14:16:31 +0100

    1. Classifier class 2. Run script

--- model.py
+++ model.py
@@ -1,7 +1,61 @@
+from datetime import datetime
+import random
+
+import numpy as np
 from scipy.io import arff
+from sklearn.metrics import confusion_matrix, roc_auc_score
 
 
-class DataLoader:
+class Classifier:
+    def __init__(self, classifier, n, x, y):
+        self.__classifier = classifier
+        self.__n = n
+        self.__x = x
+        self.__y = y
+
+        self.__fit_time_array = []
+        self.__predict_time_array = []
+
+        self.__acc_array = []
+        self.__sens_array = []
+        self.__spec_array = []
+        self.__prec_array = []
+        self.__f1_array = []
+        self.__balanced_acc_array = []
+        self.__auc_array = []
+
+    def calculate_indicators(self):
+        teach_data, test_data, teach_result, test_result = DataManager.split_data(self.__x, self.__y, 0.5)
+        for i in range(0, self.__n):
+            start_time = datetime.now()
+            self.__classifier.fit(teach_data, teach_result)
+            tick_time = datetime.now()
+            prediction = self.__classifier.predict(test_data)
+            tock_time = datetime.now()
+            self.__fit_time_array.append(tick_time - start_time)
+            self.__predict_time_array.append(tock_time - tick_time)
+            decision = self.__classifier.predict_proba(test_data)
+
+            pn, np, nn, pp = confusion_matrix(test_result, prediction).ravel()
+            accuracy = (pp + pn) / (pp + pn + np + nn)
+            sensitivity = pp / (pp + nn)
+            specificity = pn / (np + pn)
+            precision = pp / (pp + np)
+            self.__acc_array.append(accuracy)
+            self.__sens_array.append(sensitivity)
+            self.__spec_array.append(specificity)
+            self.__prec_array.append(precision)
+            self.__f1_array.append(2 * (precision * sensitivity) / (precision + sensitivity))
+            self.__balanced_acc_array.append(0.5 * (sensitivity + specificity))
+            self.__acc_array.append(roc_auc_score(test_result, decision[:, 1]))
+
+    def get_mean_result(self):
+        return np.mean(self.__acc_array), np.mean(self.__sens_array), np.mean(self.__spec_array), np.mean(self.__prec_array), \
+               np.mean(self.__f1_array), np.mean(self.__balanced_acc_array), np.mean(self.__auc_array), \
+               np.mean(self.__fit_time_array), np.mean(self.__predict_time_array)
+
+
+class DataManager:
     def __init__(self):
         pass
 
@@ -18,3 +72,9 @@ class DataLoader:
                 else:
                     x_data[w].append(data[w][k])
         return x_data, y_data
+
+    @staticmethod
+    def split_data(x, y, ratio):
+        random.shuffle(x)
+        teach_size = int(len(x) * ratio)
+        return x[:teach_size], x[teach_size:], y[:teach_size], y[teach_size:]
--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-from model import DataLoader
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 
-probes, result = DataLoader.load_data('data/diabetes.arff')
+from model import DataManager, Classifier
+
+REPETITION = 30
+
+probes, result = DataManager.load_data('data/diabetes.arff')
+x = np.array(probes)
+y = np.array(result)
+
+multinomial = Classifier(MultinomialNB(), REPETITION, x, y)
+multinomial.calculate_indicators()
+
+logistic_regression = Classifier(LogisticRegression(), REPETITION, x, y)
+logistic_regression.calculate_indicators()
+
+kneighbours_classifier = Classifier(KNeighborsClassifier(5), REPETITION, x, y)
+kneighbours_classifier.calculate_indicators()
+
+mlp_classifier = Classifier(MLPClassifier(), REPETITION, x, y)
+mlp_classifier.calculate_indicators()
+
